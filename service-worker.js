fd7fca54119f836b61ada6bf53e6ba68c11f0233
dimaPortyanka <portyanka.d@gmail.com>
2020-06-27 15:09:08 +0000

    dimaPortyanka published a site update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c3b94f879f6ce378f3db4aab5f0d6ba.js"
+  "https://dimaportyanka.github.io/movielist/precache-manifest.2b7c0b80dacb3b70239cc54b94622d2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://dimaportyanka.github.io/movielist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
